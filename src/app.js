4d6920c11bd1c4c0461a9fc24861333798c2f048
viveksharma697 <viveksharma697@gmail.com>
2022-05-21 17:43:49 +0530

    adding some styling

--- src/app.js
+++ src/app.js
@@ -147,26 +147,32 @@ export default function App() {
 		}
 	};
 	return (
-		<div className='app'>
-			{showScore ? (
-				<div className='score-section'>
-					You scored {score} out of {questions.length}
-				</div>
-			) : (
-				<>
-					<div className='question-section'>
-						<div className='question-count'>
-							<span>Question {currentQuestion + 1}</span>/{questions.length}
+		<div>
+			<div className='hedd'>
+				React-Quiz
+			</div>
+
+			<div className='app'>
+				{showScore ? (
+					<div className='score-section'>
+						You scored {score} out of {questions.length}
+					</div>
+				) : (
+					<>
+						<div className='question-section'>
+							<div className='question-count'>
+								<span>Question {currentQuestion + 1}</span>/{questions.length}
+							</div>
+							<div className='question-text'>{questions[currentQuestion].questionText}</div>
 						</div>
-						<div className='question-text'>{questions[currentQuestion].questionText}</div>
-					</div>
-					<div className='answer-section'>
-						{questions[currentQuestion].answerOptions.map((answerOption) => (
-							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
-						))}
-					</div>
-				</>
-			)}
+						<div className='answer-section'>
+							{questions[currentQuestion].answerOptions.map((answerOption) => (
+								<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+							))}
+						</div>
+					</>
+				)}
+			</div>
 		</div>
 	);
 }
